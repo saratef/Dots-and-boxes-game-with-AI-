b128376112d6941bcc06e14665031284be400a73
saratef <167128704+saratef@users.noreply.github.com>
2024-05-03 21:14:34 +0300

    Action.py

--- Dots and Boxes/game/Action.py
+++ Dots and Boxes/game/Action.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 from typing import NamedTuple, Literal, Tuple
 
 
 class Action(NamedTuple):
     Type: Literal["row", "col"]
     Position: Tuple[int, int]
-=======
-from typing import NamedTuple, Literal, Tuple
-
-
-class Action(NamedTuple):
-    Type: Literal["row", "col"]
-    Position: Tuple[int, int]
->>>>>>> f0a8fb5dfb2784b38af1cda5cd8726640744375b

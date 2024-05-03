7fcebf2ddc34d62870249781f66ddc6ba44ec86d
saratef <167128704+saratef@users.noreply.github.com>
2024-05-03 21:17:29 +0300

    State.py

--- Dots and Boxes/game/State.py
+++ Dots and Boxes/game/State.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from typing import NamedTuple
 from numpy import ndarray
 
@@ -8,14 +7,3 @@ class State(NamedTuple):
     rowStatus: ndarray
     colStatus: ndarray
     player1Turn: bool
-=======
-from typing import NamedTuple
-from numpy import ndarray
-
-
-class State(NamedTuple):
-    boardStatus: ndarray
-    rowStatus: ndarray
-    colStatus: ndarray
-    player1Turn: bool
->>>>>>> f0a8fb5dfb2784b38af1cda5cd8726640744375b
